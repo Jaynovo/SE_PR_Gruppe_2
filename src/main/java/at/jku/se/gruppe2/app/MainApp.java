ac06d7c978d87bc10760de5f611955211f275b1b
Jaynovo <41551903+Jaynovo@users.noreply.github.com>
2025-11-12 17:38:03 +0100

    Feature: Additional restructuring, added CSS and some (runnable) behaviour | time 1h
    Signed-off-by: Jaynovo <41551903+Jaynovo@users.noreply.github.com>

---
+++ src/main/java/at/jku/se/gruppe2/app/MainApp.java
@@ -0,0 +1,31 @@
+package at.jku.se.gruppe2.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+        primaryStage.setTitle("Smart Home Simulator");
+        primaryStage.setResizable(false);
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/fxml/login_page.fxml")));
+        scene.getStylesheets().add(getClass().getResource("/css/app.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        primaryStage.getScene().setRoot(FXMLLoader.load(MainApp.class.getResource("/fxml/" + fxml + ".fxml")));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
