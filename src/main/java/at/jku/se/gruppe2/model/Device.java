e45662d5b26dc67cd7efbe3a966e094a6cd4e218
Brutho99 <thomasbrunnbauer25@gmail.com>
2025-12-07 18:44:41 +0100

    Project structure: Design decision Device structure | Thomas 0.5h

--- src/main/java/at/jku/se/gruppe2/model/Device.java
+++ src/main/java/at/jku/se/gruppe2/model/Device.java
@@ -2,9 +2,12 @@ package at.jku.se.gruppe2.model;
 
 
 /* TODO currently more or less a stub class and not abstract*/
-public class Device {
+public abstract class Device {
     private int id;
     private String label;
+    private DeviceCategory category;
+    private String type;
+    private Room room;
 
 
     public int getId() {
@@ -21,5 +24,10 @@ public class Device {
 
     public void setLabel(String label) {
         this.label = label;
+    }
+
+    public enum DeviceCategory {
+        SENSOR,
+        ACTUATOR
     }
 }
---
+++ src/main/java/at/jku/se/gruppe2/model/Sensor.java
@@ -0,0 +1,7 @@
+package at.jku.se.gruppe2.model;
+
+import javafx.scene.control.Alert;
+
+public abstract class Sensor extends Device {
+
+}
