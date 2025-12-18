743e977121c90c512c9843cc12cec14139556436
Brutho99 <thomasbrunnbauer25@gmail.com>
2025-12-18 19:58:50 +0100

    Started with Thermometer implementation

--- src/main/java/at/jku/se/gruppe2/model/sensor/Thermometer.java
+++ src/main/java/at/jku/se/gruppe2/model/sensor/Thermometer.java
@@ -2,4 +2,33 @@ package at.jku.se.gruppe2.model.sensor;
 
 public class Thermometer extends Sensor {
 
+    private MeasureUnit unit;
+
+    public Thermometer() {
+        this.unit = MeasureUnit.CELSIUS; // default
+    }
+
+    public Thermometer(double initialDegree) {
+        this();
+        setValue(initialDegree);
+    }
+
+
+
+    public enum MeasureUnit {
+        CELSIUS("°C"),
+        FAHRENHEIT("°F");
+
+        private final String symbol;
+
+        MeasureUnit(String symbol) {
+            this.symbol = symbol;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+    }
 }
+
+
