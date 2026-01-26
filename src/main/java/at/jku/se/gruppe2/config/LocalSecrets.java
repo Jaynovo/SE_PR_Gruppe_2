cbd42f96e7880d124b8fe86de723ff4fcec1d203
Markus Gaber <markus.gaber@gmx.at>
2026-01-26 15:50:00 +0100

    Update ROBOFLOW_API_KEY placeholder

--- src/main/java/at/jku/se/gruppe2/config/LocalSecrets.java
+++ src/main/java/at/jku/se/gruppe2/config/LocalSecrets.java
@@ -1,5 +1,5 @@
 package at.jku.se.gruppe2.config;
 
 public class LocalSecrets {
-    public static final String ROBOFLOW_API_KEY = "U4VBu8IIJzRMSKz9UlIJ";
+    public static final String ROBOFLOW_API_KEY = "Hier API KEY einfügen";
 }
